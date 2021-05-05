6a9e8fee7687a5d9bf111df6dda0cd4ff22f4af5
sim01web <63700957+sim01web@users.noreply.github.com>
2021-05-05 19:20:59 +0530

    main1 created

---
+++ main1.java
@@ -0,0 +1,11 @@
+class Demo
+{
+  public static void main(String []s)
+  {
+    boolean x = true;
+    if(x)
+      System.out.println("x is TRUE");
+    System.out.println("x is FALSE");
+    
+  }
+} 
